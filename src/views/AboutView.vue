71cd9a998bc6fdfdf4f40c3f7e9efa8a5f922eb4
Queniex <Queniesalbiyah@gmail.com>
2023-08-18 21:24:45 +0700

    Add recommend food list

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="container">
-    <h1 class="text-center">It's About Page</h1>
+  <div class="about">
+    <Navbar />
+    <h1>About Page</h1>
+    <Footer />
   </div>
 </template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "AboutView",
+  components: {
+    Navbar,
+    Footer
+  },
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,7 +14,7 @@
                 <p>Embark on a culinary journey with our exquisite dishes that promise to delight your taste buds. Savory
                   indulgence awaits!</p>
                 <h5 class="fs-6">Start Your First Order.</h5>
-                <button class="btn btn-success"> → ORDER</button>
+                <router-link class="btn btn-success" to="/foods">→ ORDER</router-link>
               </div>
               <hr>
             </div>
@@ -22,6 +22,24 @@
               <div class="d-flex justify-content-center">
                 <img src="https://i.postimg.cc/bYKfGfnz/watering-plant-4.png" alt="" class="w-75 h-max ml-5">
               </div>
+            </div>
+          </div>
+
+          <div class="row mt-4">
+            <div class="col">
+              <h2 class="mx-3">Recommended <span class="fw-bolder text-success">Food</span>
+                <router-link class="btn btn-success float-end" to="/foods"><i class='fa fa-list-ul'></i> More
+                  List</router-link>
+              </h2>
+              <hr>
+            </div>
+            <div class="col">
+            </div>
+          </div>
+
+          <div class="row mb-3">
+            <div class="col-md-3 mt-4" v-for="product in products" :key="product.id">
+              <CardProduct :product="product" />
             </div>
           </div>
         </div>
@@ -41,34 +59,30 @@
                 <p>Embark on a culinary journey with our exquisite dishes that promise to delight your taste buds. Savory
                   indulgence awaits!</p>
                 <h5 class="fs-6">Start Your First Order.</h5>
-                <button class="btn btn-success"> → ORDER</button>
+                <router-link class="btn btn-success" to="/foods">→ ORDER</router-link>
               </div>
               <hr>
             </div>
           </div>
-        </div>
-
-        <div class="row mt-4">
-          <div class="col">
-            <h2 class="mx-3">Recommended <span class="fw-bolder text-info-emphasis">Food</span><a href=""
-                class="btn btn-success float-end"><i class='fa fa-list-ul'></i> More List</a></h2>
-            <hr>
+          <div class="row mt-4">
+            <div class="col">
+              <h2 class="mx-3">Recommended <span class="fw-bolder text-success">Food</span>
+                <router-link class="btn btn-success float-end" to="/foods"><i class='fa fa-list-ul'></i> More
+                  List</router-link>
+              </h2>
+              <hr>
+            </div>
           </div>
-          <div class="col">
-          </div>
+          <center>
+            <div class="row mb-1">
+              <div class="mt-5" v-for="product in products" :key="product.id">
+                <CardProduct :product="product" />
+              </div>
+            </div>
+          </center>
         </div>
-
-        <div class="row mb-3">
-          <div class="col-md-3 mt-4" v-for="product in products" :key="product.id">
-            <CardProduct :product="product" />
-          </div>
-        </div>
-
       </div>
     </div>
-
-
-
     <Footer />
   </div>
 </template>
